a4db7d5f125fb0eabc6d01cc7a2aa12d13d989ae
Sam Lichlyter <lichlyts@oregonstate.edu>
2016-03-08 15:26:20 -0800

    changed colors slightly and replaced video with static, placeholder image

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Kingfish Lounge</a>
+				<a class="navbar-brand" href="landingpage.php">Kingfish Lounge</a>
 			</div>
 				<div class="collapse navbar-collapse" id="site-menu">
 					<ul class="nav navbar-nav navbar-right" id="site-menu">
--- landingpage.php
+++ landingpage.php
@@ -1,10 +1,8 @@
 <!-- header.php: <html> <head> <body> -->
 <?php include_once 'header.php'; ?>
 
-	<div class="videoContainer">
-		<video muted autoplay loop>
-			<source src="http://www.le28-lille.com/wp-content/themes/le28/assets/videos/intro_close.mp4" type="video/mp4">
-		</video>
+	<div class="imageContainer">
+		<img id="landingImage" src="img/lights.png">
 	</div>
 
 <!-- footer.php: JavaScript includes </body> </html> -->
