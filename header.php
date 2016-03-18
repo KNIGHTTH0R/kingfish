62c3288d8292fb3ed71d48a64ff7f2833daba1ee
Sam Lichlyter <lichlyts@oregonstate.edu>
2016-03-18 15:28:39 -0700

    added image background to neighborhood.php, renamed landingpage.php to index.php

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="landingpage.php">Kingfish Lounge</a>
+				<a class="navbar-brand" href="index.php">Kingfish Lounge</a>
 			</div>
 				<div class="collapse navbar-collapse" id="site-menu">
 					<ul class="nav navbar-nav navbar-right" id="site-menu">
