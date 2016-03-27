9a695e088ff16463bd741db0712b6445bc2011b9
Sam Lichlyter <lichlyts@oregonstate.edu>
2016-03-27 11:55:54 -0700

    open hours is now dynamic based on day of the week; show all hours button now works as intended

--- footer.php
+++ footer.php
@@ -3,17 +3,17 @@
 
 	<div id="hours">
 		<ul id="todays-hours">
-			<li><h4>Today: 5pm &ndash; 1am</h4></li>
+			<li><h4>Today: <span class="time">5pm &ndash; 1am</span></h4></li>
 		</ul>
-		<a href="#">Show All Hours</a>
+		<a id="all-hours-button" href="#">Show All Hours</a>
 		<ul id="all-hours">
-		 	<li> Monday: 5pm &ndash; 1am </li>
-		 	<li> Tuesday: 5pm &ndash; 12am </li>
-		 	<li> Wednesday: 5pm &ndash; 1am </li>
-		 	<li> Thursday: 5pm &ndash; 1am </li>
-			<li> Friday: 5pm &ndash; 1am </li>
-			<li> Saturday: 5pm &ndash; 1am </li>
-			<li> Sunday: Closed </li>
+		 	<li id="monday"> Monday: <span class="time">5pm &ndash; 1am</span> </li>
+		 	<li id="tuesday"> Tuesday: <span class="time">5pm &ndash; 12am</span> </li>
+		 	<li id="wednesday"> Wednesday: <span class="time">5pm &ndash; 1am</span> </li>
+		 	<li id="thursday"> Thursday: <span class="time">5pm &ndash; 1am</span> </li>
+			<li id="friday"> Friday: <span class="time">5pm &ndash; 1am</span> </li>
+			<li id="saturday"> Saturday: <span class="time">5pm &ndash; 1am</span> </li>
+			<li id="sunday"> Sunday: <span class="time">Closed</span> </li>
 		</ul>
 	</div>
 
@@ -26,12 +26,34 @@
 
 </div>
 
-</body>
 <!-- INCLUDE LATEST JQUERY -->
 <script src="//code.jquery.com/jquery-2.2.1.min.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
+<script>
+	$(document).ready(function() {
+		
+		//Toggle all hours
+	    $("#all-hours-button").click(function() {
+		    $("#all-hours").toggle();
+	    });
+	    
+	    //Display today's hours
+	    var today = new Date;
+	    function getWeek(today) {
+		    weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+		    var dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+		    var day = dayNames[weekFromToday.getDay()];
+		    return day;
+	    }
+	    
+	    var time = $("#all-hours #" + getWeek(today) + " .time").text();
+	    $("#todays-hours .time").text(time);
+	});
+</script>
+
+</body>
 </html>
 <?php ob_flush(); ?>
