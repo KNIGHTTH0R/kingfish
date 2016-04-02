1c0ee672f67d5d17c7bd344c55d943c949e64ca5
Sam Lichlyter <lichlyts@oregonstate.edu>
2016-04-02 16:55:02 -0700

    added map to neighborhood.php

--- js/map.js
+++ js/map.js
@@ -3,4 +3,14 @@ L.mapbox.accessToken = 'pk.eyJ1IjoibGljayIsImEiOiI0MjQ3YjI2NGVjY2IyYzVkZTU2ODk4M
 // Create a map in the div #map
 /* L.mapbox.map('map', 'lick.7c45cff6'); */
 
-var map = L.mapbox.map('map', 'mapbox.streets').setView([44.56, -123], 12);
+var geocoder = L.mapbox.geocoder('mapbox.places'), map = L.mapbox.map('map', 'lick.pj0o7dkc');
+
+geocoder.query('151 NW Monroe Ave Suite 107, Corvallis, OR', showMap);
+
+function showMap(err, data) {
+	if (data.lbounds) {
+		map.fitBounds(data.lbounds);
+	} else if (data.latlng) {
+		map.setView([data.latlng[0], data.latlng[1]], 16);
+	}
+}
