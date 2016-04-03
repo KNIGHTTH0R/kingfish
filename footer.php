709fd1ec86d74b351221d312504fef530354352d
Sam Lichlyter <lichlyts@oregonstate.edu>
2016-04-03 14:24:39 -0700

    neighborhood page stacks on mobile

--- footer.php
+++ footer.php
@@ -34,10 +34,6 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-<!-- INCLUDE MAPBOX -->
-<script src="https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js"></script>
-<script src="js/map.js"></script>
-
 <script>
 	
 	$(document).ready(function() {
--- neighborhood.php
+++ neighborhood.php
@@ -5,6 +5,10 @@
 	<div id="map_info"></div>
 </div>
 
+<!-- INCLUDE MAPBOX -->
+<script src="https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js"></script>
+<script src="js/map.js"></script>
+
 
 <?php include_once 'footer.php' ?>
 
