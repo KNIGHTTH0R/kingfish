32a0f60f0916e6e434f89d0951e09ed4c8549e6a
Sam Lichlyter <lichlyts@oregonstate.edu>
2016-04-02 17:20:42 -0700

    temporary neighborhood.php template

--- js/map.js
+++ js/map.js
@@ -1,16 +1,4 @@
 // Provide your access token
 L.mapbox.accessToken = 'pk.eyJ1IjoibGljayIsImEiOiI0MjQ3YjI2NGVjY2IyYzVkZTU2ODk4MzhlNTJlNTk5MyJ9.aMrRUwEV0A2QYoGUlAk1Jw';
 // Create a map in the div #map
-/* L.mapbox.map('map', 'lick.7c45cff6'); */
-
-var geocoder = L.mapbox.geocoder('mapbox.places'), map = L.mapbox.map('map', 'lick.pj0o7dkc');
-
-geocoder.query('151 NW Monroe Ave Suite 107, Corvallis, OR', showMap);
-
-function showMap(err, data) {
-	if (data.lbounds) {
-		map.fitBounds(data.lbounds);
-	} else if (data.latlng) {
-		map.setView([data.latlng[0], data.latlng[1]], 16);
-	}
-}
+L.mapbox.map('map', 'lick.pj0o7dkc');
--- neighborhood.php
+++ neighborhood.php
@@ -1,6 +1,9 @@
 <?php include_once 'header.php' ?>
 
-	<iframe width='40%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/lick.pj0o7dkc/attribution,zoompan,zoomwheel,geocoder,share.html?access_token=pk.eyJ1IjoibGljayIsImEiOiI0MjQ3YjI2NGVjY2IyYzVkZTU2ODk4MzhlNTJlNTk5MyJ9.aMrRUwEV0A2QYoGUlAk1Jw'></iframe>
+<div id="map_data_container">
+	<div id="map"></div>
+	<div id="map_info"></div>
+</div>
 
 
 <?php include_once 'footer.php' ?>
