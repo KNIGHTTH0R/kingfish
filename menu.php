27bcf7d40b11ecbab4ef7032ae2f4e472981d723
Sam Lichlyter <lichlyts@oregonstate.edu>
2016-04-03 14:02:37 -0700

    if screen width is too small, menu descriptions are hidden

--- menu.php
+++ menu.php
@@ -20,20 +20,22 @@
 		<?php 	
 			$sql = "SELECT * FROM $type ORDER BY priority";
 			$query = mysqli_query($mysqli, $sql);
-			if ($query) echo "<table class='offerings'>";
-			while ($row = mysqli_fetch_assoc($query)) {
-				$name = mysqli_real_escape_string($mysqli, strip_tags($row['name']));
-				$desc = mysqli_real_escape_string($mysqli, strip_tags($row['desc']));
-				$price = mysqli_real_escape_string($mysqli, strip_tags($row['price']));
-				echo "<tr>";
-				if ($desc == NULL) {
-					echo "<td class='drink'> " . stripslashes($name) . "<td></td></td> <td class='menu_center'> $$price</td>";
-				} else {
-					echo "<td class='drink'> " . stripslashes($name) . "</td> <td class='menu_right'>  " . stripslashes($desc) . "</td> <td class='menu_center'> $$price</td>";
+			if ($query) {
+				echo "<table class='offerings'>";
+				while ($row = mysqli_fetch_assoc($query)) {
+					$name = mysqli_real_escape_string($mysqli, strip_tags($row['name']));
+					$desc = mysqli_real_escape_string($mysqli, strip_tags($row['desc']));
+					$price = mysqli_real_escape_string($mysqli, strip_tags($row['price']));
+					echo "<tr>";
+					if ($desc == NULL) {
+						echo "<td class='drink'> " . stripslashes($name) . "<td></td></td> <td class='menu_center'> $$price</td>";
+					} else {
+						echo "<td class='drink'> " . stripslashes($name) . "</td> <td class='menu_right'>  " . stripslashes($desc) . "</td> <td class='menu_center'> $$price</td>";
+					}
+					echo "<tr>";
 				}
-				echo "<tr>";
-			}
-			if ($query) echo "</table>"; 			
+				echo "</table>"; 
+			}			
 		?>
 	</div>
 <?php include_once 'footer.php'; //footer.php: </div>(class='content') <script> </body> </html> ?>
